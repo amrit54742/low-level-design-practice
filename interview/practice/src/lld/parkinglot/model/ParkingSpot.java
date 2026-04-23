81b17e87b6c628a2123136b34b5e65aae80be81b
amrit kumar <amritraj5328@gmail.com>
2026-04-23 20:41:27 +0530

    parking lot-lld done...1.1

--- interview/practice/src/lld/parkinglot/model/ParkingSpot.java
+++ interview/practice/src/lld/parkinglot/model/ParkingSpot.java
@@ -16,11 +16,13 @@ public class ParkingSpot {
     }
 
     public void assignVehicle(Vehicle vehicle) {
+        System.out.println("ParkingSpot assignVehicle with id " + this.id + " and type " + this.type);
         this.vehicle = vehicle;
         this.isFree = false;
     }
 
     public void removeVehicle() {
+        System.out.println("ParkingSpot removeVehicle with id " + this.id + " and type " + this.type);
         this.vehicle = null;
         this.isFree = true;
     }
